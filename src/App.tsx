35f81006e66c925f6cc6ac236da52b2586269d81
ta-hasunuma <ta-hasunuma@kddi-agdc.com>
2026-03-22 12:38:45 +0900

    feat: フォントとテーマ設定を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-	return <div className="font-bold text-xl text-center">Hello, World!</div>;
+	return (
+		<div className="flex min-h-svh flex-col items-center justify-center">
+			<Button variant="success">Button</Button>
+		</div>
+	);
 }
 
 export default App;
